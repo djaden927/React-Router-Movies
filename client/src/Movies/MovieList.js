1fc5c3b943cab420dfa6d689225fff575c06a0fb
djaden927 <djaden92@gmail.com>
2021-05-17 18:23:26 -0600

    finished assignment

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function MovieList(props) {
+  const { movieList } = props;
+  console.log('test movielist')
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movieList.map(movie => (
+        <Link key={movie.id} to={`/movielist/${movie.id}`}><MovieDetails  movie={movie} /></Link>
       ))}
     </div>
+
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
+  const {movie} = props;
+  console.log("testmoviedetails");
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <h2>{movie.title}</h2>
       <div className="movie-director">
-        Director: <em>{director}</em>
+        Director: <em>{movie.director}</em>
       </div>
       <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
+        Metascore: <strong>{movie.metascore}</strong>
       </div>
     </div>
   );
